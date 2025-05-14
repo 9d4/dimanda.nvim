d23d2fead2b2be723a3a456906f19ab3d8d7586c
Dimanda <d@dimanda.my.id>
2025-05-14 22:16:48 +0700

    fix issue from https://github.com/nvim-telescope/telescope.nvim/issues/3436

--- lua/dimanda/plugin/lsp.lua
+++ lua/dimanda/plugin/lsp.lua
@@ -15,13 +15,18 @@ return {
 		{ "onsails/lspkind.nvim" },
 	},
 	config = function()
+		-- issue https://github.com/nvim-telescope/telescope.nvim/issues/3436
+		vim.o.winborder = "rounded"
+
 		-- Folding
 		vim.o.foldcolumn = "1" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
+
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities.textDocument.foldingRange = {
 			dynamicRegistration = false,
